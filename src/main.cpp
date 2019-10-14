cea943751db33983ad88e1f8165cf96b48cb66c2
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2019-10-14 10:32:41 -0300

    AddFiles

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello Easy C++ project!" << std::endl;
+    cout << 10 << endl; //endl end line
+    cout << 3.14 << endl;
+    cout << true << endl;
+    cout << 'c' << endl;
+
+return 0;
+}
